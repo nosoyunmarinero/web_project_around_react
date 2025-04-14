79ce248a969461620eabbbc158df3c8544e1106b
Francisco Romero <nosoyunmarinero@gmail.com>
2025-04-13 23:20:18 -0600

    Se crearon los componentes Header,Main y Footer, estos se importaron correctamente

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+   function Footer() {
+      return (
+         <footer>
+          <p className="footer">© 2025 Around The U.S.</p>
+        </footer> 
+        );
+    }
+
+    export default Footer;
